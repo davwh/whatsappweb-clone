843a855ab4df23eba0bc2756c161ba7938ecb678
davwh <101013229+davwh@users.noreply.github.com>
2022-11-30 12:01:58 +0100

    added filter functionality

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import { Avatar } from "@material-ui/core";
 import { ExitToApp, SearchOutlined } from "@material-ui/icons";
 import {auth} from "../firebase";
@@ -12,6 +13,26 @@ import MessageIcon from '@material-ui/icons/Message';
 
 export default function Sidebar({user}) {
   const users = useUsers(user);
+
+  //Filter users
+  const [name, setName] = useState('');
+  // The search result
+  const [foundUsers, setFoundUsers] = useState(users);
+  const filter = (e) => {
+    const keyword = e.target.value;
+    if (keyword !== '') {
+      const results = users.filter((found) => {
+        return found.name.toLowerCase().startsWith(keyword.toLowerCase());
+        //toLowerCase() method to make it case-insensitive
+      });
+      setFoundUsers(results);
+    } else {
+      setFoundUsers("");
+      //If the text field is empty, show all users
+    }
+    setName(keyword);
+  };
+
 
   // Sign Out function
   function signOut(){  
@@ -34,23 +55,31 @@ export default function Sidebar({user}) {
       <MessageIcon></MessageIcon>   
       <ExitToApp className="exit" onClick={signOut} />
       </div>
-
     </div>
 
 
     <div className="sidebar__search">
       <form onSubmit={handleSubmit} className="sidebar__search--container">
       <SearchOutlined/>
-      <input placeholder="Search user" id="search" type="text" />
-      </form>
-      <div className="filter">
+      <input id="search" type="search" value={name} onChange={filter} className="input" placeholder="Search User"
+      />
+      </form>   
+       <div className="filter">
         <FilterListOutlinedIcon></FilterListOutlinedIcon>
       </div>
     </div>
+    
+    {foundUsers && foundUsers.length > 0 ? (
+      foundUsers.map((user) => (
+      <div key={user.id}></div>
+      ))
+      ) : (
+      <SidebarList title="" data={users} />
+      )}
 
-    {/*Sidebar filled with users in the database */}
-    <SidebarList title="" data={users} />
+    <SidebarList title="" data={foundUsers} />
   
   </div>
 
+  
 ;}
--- src/components/SidebarList.js
+++ src/components/SidebarList.js
@@ -1,22 +1,19 @@
-import { CircularProgress } from "@material-ui/core";
 import SidebarListItem from "./SidebarListItem";
 import "./SidebarList.css";
 
 export default function SidebarList({ title, data }) {
-  if (!data) {
+  if (!data) { 
     return (
-    <div className="loader__container sidebar__loader">
-      <CircularProgress />
+      null      
+      );
+    }
+    
+    return (
+    <div className="sidebar__chat--container">
+      <h2>{title}</h2>
+      {data.map((item) => (
+      <SidebarListItem key={item.id} item={item} />
+      ))}
       </div>
       );
     }
-             
-          return (
-          <div className="sidebar__chat--container">
-            <h2>{title}</h2>
-            {data.map((item) => (
-            <SidebarListItem key={item.id} item={item} />
-            ))}
-         </div>
-         );
-        }
